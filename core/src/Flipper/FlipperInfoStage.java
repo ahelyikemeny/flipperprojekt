ccfb6d1538560a905ae21b18bb0725a5dc7321b2
DaW33D <zsebokdavidf@gmail.com>
2021-01-22 18:18:18 +0100

    info label

--- core/src/Flipper/FlipperInfoStage.java
+++ core/src/Flipper/FlipperInfoStage.java
@@ -1,14 +1,25 @@
 package Flipper;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.MyStage;
+import hu.csanyzeg.master.MyBaseClasses.UI.MyLabel;
 
 public class FlipperInfoStage extends MyStage {
     public FlipperInfoStage(MyGame game) {
         super(new ExtendViewport(90,160), game);
         addBackButtonScreenBackByStackPopListener();
-    }
-}
+        setCameraResetToCenterOfScreen();
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = game.getMyAssetManager().getFont("alegreyaregular.otf");
+        labelStyle.fontColor = Color.GREEN;
+        MyLabel label = new MyLabel(game, "Kancsal csinalj texturat", labelStyle);
+        label.setFontScale(0.2f);
+        label.setPosition(20,70);
+        addActor(label);
+
+} }
